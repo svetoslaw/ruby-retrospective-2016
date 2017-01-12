8ff0f36829390d9f6bb72ff35e47ce6d7c1c86cc
svetoslaw <svetoslaw4o@gmail.com>
2017-01-12 15:23:18 +0200

    remove keys_to_strings function and instead change the element looked for

--- tasks/2/solution.rb
+++ tasks/2/solution.rb
@@ -1,20 +1,12 @@
 class Hash
   def fetch_deep(path)
-    first_element_key, split_path = path.split('.', 2)
-    hash = self.keys_to_strings
-    if split_path && hash[first_element_key]
-      hash[first_element_key].fetch_deep(split_path) 
+    first_element, split_path = path.split('.', 2)
+    first_element = first_element.to_sym if self[first_element.to_sym]
+    if split_path && self[first_element]
+      self[first_element].fetch_deep(split_path) 
     else
-      hash[first_element_key]
+      self[first_element]
     end
-  end
-
-  def keys_to_strings
-    result = {}
-    self.each do |key, value|
-      result[key.to_s] = value
-    end
-    result
   end
 
   def reshape(shape)
@@ -39,11 +31,12 @@ class Array
   end
 
   def fetch_deep(path)
-    first_element_key, split_path = path.split('.', 2)
-    if split_path && hash[first_element_key.to_i]
-      self[first_element_key.to_i].fetch_deep(split_path)
+    first_element, split_path = path.split('.', 2)
+    first_element = first_element.to_i
+    if split_path && hash[first_element]
+      self[first_element].fetch_deep(split_path)
     else
-      self[first_element_key.to_i]
+      self[first_element]
     end
   end
 end
