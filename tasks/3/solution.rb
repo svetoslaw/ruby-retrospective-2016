6b98450f8a91042084d68e2007a1023216d935d1
svetoslaw <svetoslaw4o@gmail.com>
2017-01-16 16:55:53 +0200

    make separate classes for the options and arguments

--- tasks/3/solution.rb
+++ tasks/3/solution.rb
@@ -1,66 +1,90 @@
+class Argument
+  def initialize(name, block)
+    @name = name
+    @block = block
+  end
+
+  def parse(command_runner, argument)
+    @block.call(command_runner, argument)
+  end
+end
+
+class Option
+  attr_accessor :short_name, :full_name
+  def initialize(short_name, full_name, param, block)
+    @short_name = short_name
+    @full_name = full_name
+    @parameters = param
+    @block = block
+  end
+
+  def parse(command_runner, parameter = true)
+    @block.call(command_runner, parameter)
+  end
+end
+
 class CommandParser
   def initialize(command_name)
     @command_name = command_name
-    @command_args = []
-    @options = {}
+    @arguments = []
+    @options = []
     @args_help = "Usage: #{@command_name}"
     @options_help = ''
   end
 
   def argument(name, &block)
-    @command_args << block
+    @arguments << Argument.new(name, block)
     @args_help << " [#{name}]"
   end
 
   def option(short_name, full_name, help_text, &block)
-    @options[short_name] = block
-    @options[full_name] = block
+    @options << Option.new(short_name, full_name, nil, block)
     @options_help << "\n    -#{short_name}, --#{full_name} #{help_text}"
   end
 
   def option_with_parameter(short_name, full_name, help_text, param, &block)
-    @options[short_name] = block
-    @options[full_name] = block
+    @options << Option.new(short_name, full_name, param, block)
     @options_help << "\n    -#{short_name}, --#{full_name}=#{param} "
     @options_help << help_text
-  end
-
-  def parse(command_runner, argv)
-    command_args_dup = @command_args.dup
-    argv.each do |argument|
-      block_caller(argument, command_runner, command_args_dup)
-    end
   end
 
   def help
     @args_help + @options_help
   end
 
+  def parse(command_runner, argv)
+    arguments_dup = @arguments.dup
+    argv.each do |arg|
+      if arg.start_with?('--')
+        long_name_option_call(command_runner, arg)
+      elsif arg.start_with?('-')
+        short_name_option_call(command_runner, arg)
+      else
+        arguments_dup.shift.parse(command_runner, arg)
+      end
+    end
+  end
+
   private
-  def block_caller(argument, command_runner, command_args)
-    if argument[0..1] == '--'
-      full_name_option_call(argument, command_runner)
-    elsif argument[0] == '-'
-      short_name_option_call(argument, command_runner)
-    else
-      command_args.shift.call(command_runner, argument)
-    end
-  end
 
-  def full_name_option_call(argument, command_runner)
-    call_parameters = argument[2..-1].split('=')
-    if @options.key?(call_parameters[0])
+  def long_name_option_call(command_runner, arg)
+    call_parameters = arg[2..-1].split('=')
+    @options.each do |opt|
+      next unless opt.full_name == call_parameters[0]
+      
       call_parameters[1] = true unless call_parameters[1]
-      @options[call_parameters[0]].call(command_runner, call_parameters[1])
+      opt.parse(command_runner, call_parameters[1])
     end
   end
 
-  def short_name_option_call(argument, command_runner)
-    option_name = argument[1]
-    if @options.key?(option_name)
-      option_value = argument[2..-1]
-      option_value = true if option_value == ''
-      @options[option_name].call(command_runner, option_value)
+  def short_name_option_call(command_runner, arg)
+    @options.each do |opt|
+      next unless opt.short_name == arg[1]
+
+      opt.short_name == arg[1]
+      option_parameter = arg[2..-1]
+      option_parameter = true if option_parameter == ''
+      opt.parse(command_runner, option_parameter)
     end
   end
 end
